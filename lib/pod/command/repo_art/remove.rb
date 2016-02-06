8641ef7131c75e12a7e57ec4cc2ee3ab3db10e3a
danf <danf@jfrog.com>
2016-02-06 20:18:18 +0200

    Fix remove help banner

--- lib/pod/command/repo_art/remove.rb
+++ lib/pod/command/repo_art/remove.rb
@@ -5,18 +5,24 @@ module Pod
         self.summary = 'Remove an Artifactory-backed Specs repo'
 
         self.description = <<-DESC
-          Deletes the Spec repo called `NAME` from the local spec-repos directory at `~/.cocoapods/repos/.`
+          Deletes the Spec repo called 'NAME' from the local spec-repos directory at '~/.cocoapods/repos/.'
         DESC
 
         self.arguments = [
-            CLAide::Argument.new('NAME', true),
+            CLAide::Argument.new('NAME', true)
         ]
 
         def initialize(argv)
+          @name = argv.shift_argument
           @cmd = Command::Repo::Remove.new(argv)
+          super
         end
 
         def validate!
+          super
+          unless @name
+            help! 'This command requires a repo name.'
+          end
           @cmd.validate!
         end
 
