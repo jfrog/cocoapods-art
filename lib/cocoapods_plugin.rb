cf0849916a6f88042d7d50eb634c1890e901dbe7
杨萧玉 <yulingtianxia@users.noreply.github.com>
2021-06-27 14:28:38 +0800

    fix: adding custom cdn source failed. (#50)

--- lib/cocoapods_plugin.rb
+++ lib/cocoapods_plugin.rb
@@ -129,9 +129,12 @@ module Pod
           def source_from_path(path)
             @sources_by_path ||= Hash.new do |hash, key|
               art_repo = "#{UTIL.get_repos_art_dir()}/#{key.basename}"
-              hash[key] = if key.basename.to_s == Pod::TrunkSource::TRUNK_REPO_NAME
+              hash[key] = case
+                          when key.basename.to_s == Pod::TrunkSource::TRUNK_REPO_NAME
                             TrunkSource.new(key)
-                          elsif File.exist?("#{art_repo}/.artpodrc")
+                          when (key + '.url').exist?
+                            CDNSource.new(key)
+                          when File.exist?("#{art_repo}/.artpodrc")
                             create_source_from_name(key.basename)
                           else
                             Source.new(key)
